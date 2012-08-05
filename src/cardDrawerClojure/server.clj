d5e73f5d8123d1bf7fb692eb36eea31ea78ed401
Anders Karlsen <karlsenanders@gmail.com>
2012-08-05 12:41:04 +0200

    Added update form

--- src/cardDrawerClojure/server.clj
+++ src/cardDrawerClojure/server.clj
@@ -1,6 +1,8 @@
 (ns cardDrawerClojure.server
   (:use [noir.core :only [defpage defpartial]]
         [hiccup.page-helpers :only [html5 include-js]]
+        [hiccup.form-helpers]
+        [noir.response :only [redirect]]
         )
   (:require [noir.server :as server])
 )
@@ -11,13 +13,31 @@
   [:div {:id "someid"} (str "Date is " (new java.util.Date) " Value is " value)]
   )
 
+(defpartial update-form []
+  (form-to [:post "/updatevalue"]
+     (label "newval" "New value:")
+     (text-field "newval")
+     (submit-button "Update value")
+  ))
+
+(defn to-int [s]
+  (try (Integer/parseInt s) (catch NumberFormatException e nil)))
+
+(defpage [:post "/updatevalue"] {:as updateobject}
+  (let [aval (to-int (updateobject :newval))]
+    (if (not (nil? aval)) (dosync (ref-set counter aval))) 
+  )
+  (redirect "/")
+  )
+
 (defpage "/" []
     (html5 
       [:head
     [:title "Dummy title"]
     (include-js "/jquery-1.7.2.js") (include-js "/reload.js")]
-      [:body [:h1 "Headline"] (status-content @counter)])
+      [:body [:h1 "Headline"] (status-content @counter) (update-form)])
 )
+
 
 (defpage "/update.html" []
   (status-content @counter)
