0cfde61b5922f75aa97363bbf3890f7696653d88
Anders Karlsen <karlsenanders@gmail.com>
2012-08-05 21:37:01 +0200

    Drawing card

--- src/cardDrawerClojure/core.clj
+++ src/cardDrawerClojure/core.clj
@@ -1,6 +1,31 @@
 (ns cardDrawerClojure.core)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defn register-player [game name]
+  (assoc game :players (assoc (game :players) name []))
+)
+
+(defn update-player-deck [game player new-hand]
+  (assoc game :players (assoc (game :players) player new-hand))
+  )
+
+(defn remove-item [listing number]
+  (vec (concat (subvec (vec listing) 0 number) 
+        (if (= (inc number) (count listing)) [] (subvec (vec listing) (inc number))))
+  ))
+
+
+(defn draw-card [game player]
+  (let [hand ((game :players) player) deck (game :deck) pick (rand-int (count (game :deck)))]
+  (update-player-deck (assoc game :deck (remove-item deck pick)) player (conj hand (deck pick)))
+
+  ))
+
+
+
+(defn pick-random [listing picks]
+  (if (<= picks 0) []
+  (let [number (rand-int (count listing))]
+  (cons ((vec listing) number) (pick-random 
+        (remove-item listing number)  
+        (dec picks)))
+  )))
--- test/cardDrawerClojure/core_test.clj
+++ test/cardDrawerClojure/core_test.clj
@@ -2,6 +2,14 @@
   (:use clojure.test
         cardDrawerClojure.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest register-test
+  (testing "That one can register and start a new game"
+    (is (= {:players {"Darth" []}} (register-player {:players {}} "Darth")) "First player")
+    (is (= {:players {"Darth" [] "Luke" []}} (register-player {:players {"Darth" []}} "Luke")) "Second player")
+))
+
+(deftest draw-card-test
+  (testing "Drawing cards"
+    (is (= {:players {"Darth" [1]} :deck []} (draw-card {:players {"Darth" []} :deck [1]} "Darth")) "Drawing the last card")
+    )
+  )
