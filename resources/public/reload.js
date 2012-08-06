239bac7ea95131bdab0993b558138ab110f3044f
Anders Karlsen <karlsenanders@gmail.com>
2012-08-06 23:07:50 +0200

    Adjusted reload time

--- resources/public/reload.js
+++ resources/public/reload.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
    var refreshId = setInterval(function() {
       $("#someid").load('update.html?name=' + document.getElementById('namediv').innerHTML);
-   }, 3000);
+   }, 10000);
    $.ajaxSetup({ cache: false });
 });
